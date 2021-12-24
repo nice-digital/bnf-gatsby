64f0a216e0b3ed147791e1c2c020010c718fbfcf
Ian Routledge <ian@ediblecode.com>
2021-12-24 18:48:11 +0000

    Add homepage panels

--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -2,7 +2,10 @@ import { Link } from "gatsby";
 import { FC } from "react";
 
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { Button } from "@nice-digital/nds-button";
+import { Grid, GridItem } from "@nice-digital/nds-grid";
 import { Hero } from "@nice-digital/nds-hero";
+import { Panel } from "@nice-digital/nds-panel";
 
 import { Layout } from "@/components/Layout/Layout";
 import { SEO } from "@/components/SEO/SEO";
@@ -28,13 +31,48 @@ const IndexPage: FC = () => {
 						<Breadcrumb>{siteTitleShort}</Breadcrumb>
 					</Breadcrumbs>
 				}
+				actions={
+					<Button to="/drugs/" variant="cta" elementType={Link}>
+						Browse drugs
+					</Button>
+				}
 			/>
 
-			<p>
-				<Link to="/drugs/">Go to drugs</Link>
-				<br />
-				<Link to="/about/">Go to about</Link>
-			</p>
+			<Grid gutter="loose">
+				<GridItem cols={12} sm={6} md={4}>
+					<Panel variant="impact-alt">
+						<h2 className="h3">Drugs A&nbsp;to&nbsp;Z</h2>
+						<p>Browse the list of drug monographs, arranged alphabetically.</p>
+						<Button to="/drugs/" variant="cta" elementType={Link}>
+							Browse drugs
+						</Button>
+					</Panel>
+				</GridItem>
+				<GridItem cols={12} sm={6} md={4}>
+					<Panel variant="primary">
+						<h2 className="h3">Interactions</h2>
+						<p>
+							Browse the list of drug interactions, arranged alphabetically.
+						</p>
+						<Button to="/drugs/" variant="inverse" elementType={Link}>
+							Browse interactions
+						</Button>
+					</Panel>
+				</GridItem>
+				<GridItem cols={12} sm={6} md={4}>
+					<Panel variant="supporting">
+						<h2 className="h3">Treatment summaries</h2>
+						<p>
+							Browse the list of treatment summaries, arranged alphabetically.
+						</p>
+						<Button to="/drugs/" variant="secondary" elementType={Link}>
+							Browse treatment summaries
+						</Button>
+					</Panel>
+				</GridItem>
+			</Grid>
+
+			<Link to="/about/">Go to about</Link>
 		</Layout>
 	);
 };
