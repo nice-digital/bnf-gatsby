21488b023c9a0340aa8e0121c0a66af6074ffd53
Ian Routledge <ian@ediblecode.com>
2021-12-24 18:47:40 +0000

    Add util comment

--- gatsby/plugins/gatsby-source-bnf/src/node-creation/utils.ts
+++ gatsby/plugins/gatsby-source-bnf/src/node-creation/utils.ts
@@ -14,6 +14,14 @@ export type TypedNodeInput<
 		} & NodeInput["internal"];
 	};
 
+/**
+ * Helper method to create a BNF-specific Gatsby GraphQL Node, that is, a node with a type starting with `Bnf`.
+ *
+ * @param nodeContent The content of the node to create, without any of the Gatsby internals
+ * @param idField The property within `nodeContent`, that returns a string value, that's used internally for the generated node id via Gatsby's `createNodeId`
+ * @param type The name of the node type, starting with `Bnf` e.g. `BnfDrug`
+ * @template TNodeContent The type of the node content
+ */
 export const createBnfNode = <TNodeContent>(
 	nodeContent: TNodeContent,
 	idField: ConditionalKeys<TNodeContent, string>,
