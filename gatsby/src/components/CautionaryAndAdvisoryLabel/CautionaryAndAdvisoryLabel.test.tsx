e200c4c0ed9062a8c0e267c84cf9152a2fd3713b
Chris Barker <chris@christopherbarker.co.uk>
2022-01-19 13:58:56 +0000

    BNF-1180 - Updated label tests

--- gatsby/src/components/CautionaryAndAdvisoryLabel/CautionaryAndAdvisoryLabel.test.tsx
+++ gatsby/src/components/CautionaryAndAdvisoryLabel/CautionaryAndAdvisoryLabel.test.tsx
@@ -1,7 +1,40 @@
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
+import {
+	CautionaryAndAdvisoryLabel,
+	type CautionaryAndAdvisoryLabelProps,
+} from "./CautionaryAndAdvisoryLabel";
+
+const props: CautionaryAndAdvisoryLabelProps = {
+	description: "<p>Test description</p>",
+	englishRecommendation: "English test recommendation",
+	welshRecommendation: "Welsh test recommendation",
+	number: 3,
+};
+
 describe("CautionaryAndAdvisoryLabel", () => {
-	it.todo("Should render a heading containing the label number");
-	it.todo("Should render both English and Welsh recommendations");
-	it.todo("Should contain an HTML description of the label");
+	it("Should render a heading containing the label number", () => {
+		render(<CautionaryAndAdvisoryLabel {...props} />);
+		const heading = screen.getByRole("heading", { name: "Label 3" });
+		expect(heading).toBeInTheDocument();
+	});
+
+	it("Should contain an English language recommendation", () => {
+		render(<CautionaryAndAdvisoryLabel {...props} />);
+		const recommendation = screen.getByText(props.englishRecommendation);
+		expect(recommendation).toHaveAttribute("lang", "en-GB");
+	});
+
+	it("Should contain a Welsh language recommendation", () => {
+		render(<CautionaryAndAdvisoryLabel {...props} />);
+		const recommendation = screen.getByText(props.welshRecommendation);
+		expect(recommendation).toHaveAttribute("lang", "cy");
+	});
+
+	it("Should contain an HTML description of the label", () => {
+		render(<CautionaryAndAdvisoryLabel {...props} />);
+		const description = screen.getByText("Test description");
+		expect(description).toBeInTheDocument();
+	});
 });
