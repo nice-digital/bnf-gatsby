b8e78a745e1e35643e700083cd25f46dc6e9d7de
Ian Routledge <ian@ediblecode.com>
2022-02-01 21:29:53 +0000

    BNF-1208 Add feed details

--- gatsby/plugins/gatsby-source-bnf/src/downloader/downloader.ts
+++ gatsby/plugins/gatsby-source-bnf/src/downloader/downloader.ts
@@ -2,8 +2,29 @@ import axios from "axios";
 
 import type { Feed } from "./types";
 
-export const downloadFeed = async (feedURL: string): Promise<Feed> => {
-	const response = await axios.get<Feed>(feedURL);
+export interface PluginOptions {
+	/** The API base URL */
+	feedURL: string;
+	/** API key for authenticating against the feed endpoints */
+	userKey: string;
+	/** Which site we're currently building */
+	site: "bnf" | "bnfc";
+}
 
-	return response.data;
+export const downloadFeed = async ({
+	site,
+	feedURL,
+	userKey,
+}: PluginOptions): Promise<Feed> => {
+	const url = `${site}/publication`;
+
+	const { data } = await axios.get<Feed>(url, {
+		baseURL: feedURL,
+		params: {
+			format: "niceJson",
+			user_key: userKey,
+		},
+	});
+
+	return data;
 };
