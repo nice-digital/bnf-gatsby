a06e749668a4deef4093c2ec17d3b7af38ddcba9
Ian Routledge <ian.routledge@nice.org.uk>
2022-02-02 10:05:20 +0000

    BNF-1208 Add feed details (#9)

--- gatsby/plugins/gatsby-source-bnf/src/downloader/downloader.test.ts
+++ gatsby/plugins/gatsby-source-bnf/src/downloader/downloader.test.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-import { downloadFeed } from "./downloader";
+import { downloadFeed, PluginOptions } from "./downloader";
 
 jest.mock("axios");
 
 describe("downloader", () => {
+	const pluginOptions: PluginOptions = {
+		feedURL: "https://someurl.com/v2/",
+		site: "bnf",
+		userKey: "abc123",
+	};
+
 	it("should download JSON data from feed URL", async () => {
 		const mockJsonResponseBody = {
 			test: true,
@@ -14,9 +20,12 @@ describe("downloader", () => {
 			data: mockJsonResponseBody,
 		});
 
-		const returnedData = await downloadFeed("A URL");
+		const returnedData = await downloadFeed(pluginOptions);
 
-		expect(axios.get).toHaveBeenCalledWith("A URL");
+		expect(axios.get).toHaveBeenCalledWith("bnf/publication", {
+			baseURL: "https://someurl.com/v2/",
+			params: { format: "niceJson", user_key: "abc123" },
+		});
 		expect(returnedData).toBe(mockJsonResponseBody);
 	});
 });
