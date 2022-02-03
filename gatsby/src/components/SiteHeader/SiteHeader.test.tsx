7ab41c8338ceb3a679e716ab56b711dd46d131b1
John Davey <john.davey@nice.org.uk>
2022-02-03 16:17:23 +0000

    unit testing wip

--- gatsby/src/components/SiteHeader/SiteHeader.test.tsx
+++ gatsby/src/components/SiteHeader/SiteHeader.test.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/no-node-access */
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { useSiteMetadata } from "@/hooks/useSiteMetadata";
 
@@ -16,23 +17,69 @@ const mockAutocompleteEndPointSuggestionsForDrug = [
 	},
 ];
 
+const mockNoLinkorTitle = [
+	{
+		TypeAheadType: "Drug",
+	},
+];
+
 describe("SiteHeader", () => {
 	describe("Autocomplete", () => {
-		it("should apply the correct forumlary prefix for autocomplete results", async () => {
+		it("should apply the BNF forumlary prefix for autocomplete results", async () => {
 			(useSiteMetadata as jest.Mock).mockImplementationOnce(() => ({
-				isBNF: false,
+				isBNF: true,
 			}));
+
 			render(<SiteHeader />);
 
 			// Global nav uses a fetch to load autocomplete suggestions, and changing the input value triggers this fetch
 			fetchMock.mockResponseOnce(
 				JSON.stringify(mockAutocompleteEndPointSuggestionsForDrug)
 			);
+			userEvent.type(await screen.findByRole("combobox"), "SODIUM");
+
 			await waitFor(() => {
-				expect(screen.getByText(/Error page content/i)).toBeInTheDocument();
+				const suggestedElements = screen.queryAllByRole("option");
+				expect(suggestedElements[0].textContent).toEqual(
+					"SODIUM BICARBONATE (BNF drugs/monographs)"
+				);
 			});
 		});
-		it.todo("should not return a template if there is no suggestion or link");
+		it("should apply the BNFC forumlary prefix for autocomplete results", async () => {
+			(useSiteMetadata as jest.Mock).mockImplementationOnce(() => ({
+				isBNF: false,
+			}));
+
+			render(<SiteHeader />);
+
+			// Global nav uses a fetch to load autocomplete suggestions, and changing the input value triggers this fetch
+			fetchMock.mockResponseOnce(
+				JSON.stringify(mockAutocompleteEndPointSuggestionsForDrug)
+			);
+			userEvent.type(await screen.findByRole("combobox"), "SODIUM");
+
+			await waitFor(() => {
+				const suggestedElements = screen.queryAllByRole("option");
+				expect(suggestedElements[0].textContent).toEqual(
+					"SODIUM BICARBONATE (BNFC drugs/monographs)"
+				);
+			});
+		});
+		it.only("should not return a template if there is no suggestion or link", async () => {
+			(useSiteMetadata as jest.Mock).mockImplementationOnce(() => ({
+				isBNF: false,
+			}));
+
+			render(<SiteHeader />);
+
+			fetchMock.mockResponseOnce(JSON.stringify(mockNoLinkorTitle));
+			userEvent.type(await screen.findByRole("combobox"), "FRANGIPAN");
+			await waitFor(() => {
+				const suggestedElements = screen.queryAllByRole("option");
+				expect(suggestedElements).toHaveLength(1);
+				// expect(suggestedElements.firstChild).toBeNull();
+			});
+		});
 		it.todo("should return the correct label(s)");
 		it.todo("should have a correctly formatted url for autocomplete queries");
 	});
