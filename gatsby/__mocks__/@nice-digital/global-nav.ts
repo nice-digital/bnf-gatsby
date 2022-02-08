0211f9261ba71b7cd50b7a11db08a297ddda46f9
John Davey <john.davey@nice.org.uk>
2022-02-08 10:18:33 +0000

    BNF-1221 Refactor automock for global nav  header. Add tests for labels.

--- gatsby/__mocks__/@nice-digital/global-nav.ts
+++ gatsby/__mocks__/@nice-digital/global-nav.ts
@@ -1,2 +1,4 @@
+// Actual header required because the SiteHeader component is mocked globally (see jest.setup.ts)
+export const Header = jest.requireActual("@nice-digital/global-nav").Header;
+// Mocked footer as null to avoid noise in tests
 export const Footer = jest.fn().mockReturnValue(null);
-export const Header = jest.fn().mockReturnValue(null);
--- gatsby/src/components/SiteHeader/SiteHeader.test.tsx
+++ gatsby/src/components/SiteHeader/SiteHeader.test.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/no-node-access */
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { useSiteMetadata } from "@/hooks/useSiteMetadata";
 
 // Header is mocked globally in setup
 const { SiteHeader } = jest.requireActual("./SiteHeader");
-const { Header } = jest.requireActual("@nice-digital/global-nav");
-const GlobalNavHeader = Header;
 
 const mockAutocompleteEndPointSuggestionsForDrug = [
 	{
@@ -19,18 +17,14 @@ const mockAutocompleteEndPointSuggestionsForDrug = [
 	},
 ];
 
-const mockNoLinkorTitle = [
-	{
-		TypeAheadType: "Drug",
-	},
-];
+const useSiteMetadataMock = useSiteMetadata as jest.Mock;
 
 describe("SiteHeader", () => {
 	describe("Autocomplete", () => {
-		it.only("should apply the BNF forumlary prefix for autocomplete results", async () => {
-			(useSiteMetadata as jest.Mock).mockImplementationOnce(() => ({
+		it("should apply the BNF forumlary prefix for autocomplete results for BNF", async () => {
+			useSiteMetadataMock.mockReturnValueOnce({
 				isBNF: true,
-			}));
+			});
 
 			render(<SiteHeader />);
 
@@ -47,10 +41,10 @@ describe("SiteHeader", () => {
 				);
 			});
 		});
-		it("should apply the BNFC forumlary prefix for autocomplete results", async () => {
-			(useSiteMetadata as jest.Mock).mockImplementationOnce(() => ({
+		it("should apply the BNFC forumlary prefix for autocomplete results for BNFC", async () => {
+			useSiteMetadataMock.mockReturnValueOnce({
 				isBNF: false,
-			}));
+			});
 
 			render(<SiteHeader />);
 
@@ -67,21 +61,37 @@ describe("SiteHeader", () => {
 				);
 			});
 		});
-		it("should not return a template if there is no suggestion or link", async () => {
-			(useSiteMetadata as jest.Mock).mockImplementationOnce(() => ({
-				isBNF: false,
-			}));
 
-			render(<SiteHeader />);
-
-			fetchMock.mockResponseOnce(JSON.stringify(mockNoLinkorTitle));
-			userEvent.type(await screen.findByRole("combobox"), "zyx");
-			await waitFor(() => {
-				const suggestedElements = screen.queryAllByRole("option");
-				expect(suggestedElements).toHaveLength(1);
-			});
-		});
-		it.todo("should append the correct typeahead label(s) for suggestions");
+		//assert against fetchmock...to have been called with
 		it.todo("should have a correctly formatted url for autocomplete queries");
+
+		it.todo("test anchor href=link...");
+
+		it.each([
+			["Drug", "BNF drugs/monographs", true],
+			["Drug", "BNFC drugs/monographs", false],
+		])(
+			"should show label for %s typeahead suggestions - %s",
+			async (TypeAheadType, expected, isBNF) => {
+				// expect(input * 2).toBe(expected);
+				useSiteMetadataMock.mockReturnValueOnce({
+					isBNF,
+				});
+
+				render(<SiteHeader />);
+
+				// Global nav uses a fetch to load autocomplete suggestions, and changing the input value triggers this fetch
+				fetchMock.mockResponseOnce(
+					JSON.stringify([{ Title: "test", Link: "/test", TypeAheadType }])
+				);
+				userEvent.type(await screen.findByRole("combobox"), "anything");
+
+				await waitFor(() => {
+					const form = screen.getByRole("search");
+					const suggestedElement = within(form).queryByRole("option");
+					expect(suggestedElement).toHaveTextContent(`test (${expected})`);
+				});
+			}
+		);
 	});
 });
