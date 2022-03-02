6570a7f5badf1af6337287e04e660e2a203be6e2
Ian Routledge <ian@ediblecode.com>
2022-03-02 20:58:45 +0000

    BNF-1236 Fix spelling mistakes

--- gatsby/plugins/gatsby-source-bnf/index.ts
+++ gatsby/plugins/gatsby-source-bnf/index.ts
@@ -1,8 +1,8 @@
 /**
  * Exporting things here, in combination with a custom path alias (see tsconfig.json)
- * allowe use to do things like:
+ * allows us to do things like the following inside components and pages:
+ *
  * import { FeedPrep } from "@nice-digital/gatsby-source-bnf";
- * Inside components etc.
  *
  * This allows us to re-use the types from the feed and avoid duplicating type definitions.
  */
