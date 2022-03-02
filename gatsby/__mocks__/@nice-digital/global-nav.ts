e979357f79edbc81f9381204a9897b3e03cce4d2
John Davey <john.davey@nice.org.uk>
2022-03-02 11:15:15 +0000

    BNF-1181 Add global nav main export to mocks to resolve issue with unit tests failing en masse

--- gatsby/__mocks__/@nice-digital/global-nav.ts
+++ gatsby/__mocks__/@nice-digital/global-nav.ts
@@ -2,3 +2,5 @@
 export const Header = jest.requireActual("@nice-digital/global-nav").Header;
 // Mocked footer as null to avoid noise in tests
 export const Footer = jest.fn().mockReturnValue(null);
+// Actual main component required
+export const Main = jest.requireActual("@nice-digital/global-nav").Main;
