9d7768d4f2ae300a50999614979df5dbffee196d
John Davey <john.davey@nice.org.uk>
2022-03-14 12:53:50 +0000

    BNF-1255 Add further unit tests for data layer

--- gatsby/__tests__/gatsby-browser.test.ts
+++ gatsby/__tests__/gatsby-browser.test.ts
@@ -1,33 +1,93 @@
-import { type Location } from "@reach/router";
+import { waitFor } from "@testing-library/react";
 import { type RouteUpdateArgs } from "gatsby";
 
 import { onRouteUpdate } from "../gatsby-browser";
 
-const test = {
-	prevLocation: {
-		href: "http://localhost:8000/elsewhere/",
-		pathname: "/elsewhere/",
-	},
-} as RouteUpdateArgs;
-
 describe("gatsby-browser", () => {
 	describe("onRouteUpdate behaviour", () => {
+		let spy: jest.SpyInstance;
 		beforeEach(() => {
 			window.dataLayer = [];
+			spy = jest
+				.spyOn(window, "requestAnimationFrame")
+				.mockImplementation((callback: FrameRequestCallback): number => {
+					callback(0);
+					return 0;
+				});
 		});
 
-		it.todo("request animationframe");
+		afterEach(() => {
+			spy.mockRestore();
+		});
 
-		it.todo("should push a page view to the data layer when the path changes");
-
-		it.only("Should push the prevLocation to the data layer", () => {
+		it("Should use request animationframe delay before push to the data layer", async () => {
 			onRouteUpdate({
-				prevLocation: null,
+				prevLocation: { href: "elsewhere", pathname: "/elsewhere" },
+				location: { href: "somewhere", pathname: "/somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			expect(requestAnimationFrame).toHaveBeenCalledTimes(2);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(2);
+			});
+		});
+
+		it("should push a page view to the data layer when the path changes", async () => {
+			onRouteUpdate({
+				prevLocation: { href: "elsewhere", pathname: "/elsewhere" },
+				location: { href: "somewhere", pathname: "/somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(2);
+			});
+
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere",
+				referrer: "elsewhere",
+			});
+
+			expect(window.dataLayer[1]).toStrictEqual({
+				event: "pageview",
+			});
+		});
+
+		it("should NOT push a page view to the data layer when the path does NOT change", async () => {
+			onRouteUpdate({
+				prevLocation: { href: "somewhere", pathname: "/somewhere" },
+				location: {
+					href: "somewhere#someanchor",
+					pathname: "/somewhere",
+				},
+			} as unknown as RouteUpdateArgs);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(1);
+			});
+
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere#someanchor",
+				referrer: "somewhere",
+			});
+		});
+
+		it("Should push the prevLocation to the data layer", async () => {
+			onRouteUpdate({
+				prevLocation: {
+					href: "elsewhere",
+				},
 				location: { href: "somewhere" },
 			} as unknown as RouteUpdateArgs);
 
-			console.log("###", window.dataLayer);
-			expect(window.dataLayer).toHaveLength(1);
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(1);
+			});
+
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere",
+				referrer: "elsewhere",
+			});
 		});
 
 		it("Should push the current location to the data layer", () => {
@@ -58,5 +118,3 @@ describe("gatsby-browser", () => {
 		});
 	});
 });
-
-// expect(window.dataLayer[0]).toStrictEqual({ event: "pageview" });
--- gatsby/gatsby-browser.ts
+++ gatsby/gatsby-browser.ts
@@ -11,14 +11,6 @@ export const onRouteUpdate = ({
 		const prevPath = (prevLocation as Location).pathname,
 			path = location.pathname;
 
-		console.log(
-			"prevLocation ",
-			prevLocation,
-			"prevPath",
-			prevPath,
-			" path ",
-			path
-		);
 		// Push our own event to the dataLayer on page change rather than using the
 		// 'gatsby-route-change' event built into gatsby-plugin-google-tagmanager.
 		// Because gatsby-route-change is pushed on initial page load as well as route change,
