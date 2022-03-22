108905c8967146372435dec053178613c2dbacd1
Ian Routledge <ian@ediblecode.com>
2022-03-22 16:16:33 +0000

    BNF-1271 Add constituent tests

--- gatsby/plugins/gatsby-source-bnf/src/node-creation/drugs.ts
+++ gatsby/plugins/gatsby-source-bnf/src/node-creation/drugs.ts
@@ -1,25 +1,22 @@
 import { type SourceNodesArgs } from "gatsby";
-import { type Except } from "type-fest";
+import { type Merge } from "type-fest";
 
 import { type PHPID, type SID, type FeedDrug } from "../downloader/types";
 import { BnfNode } from "../node-types";
 
 import { createBnfNode } from "./utils";
 
-export type DrugNodeInput = Except<
+export type DrugNodeInput = Merge<
 	FeedDrug,
-	| "id"
-	| "primaryClassification"
-	| "secondaryClassifications"
-	| "constituentDrugs"
-> & {
-	id: SID;
-	phpid: PHPID;
-	constituentDrugs?: {
-		message: string;
-		constituents: SID[];
-	};
-};
+	{
+		id: SID;
+		phpid: PHPID;
+		constituentDrugs?: {
+			message: string;
+			constituents: SID[];
+		};
+	}
+>;
 
 export const createDrugNodes = (
 	drugs: FeedDrug[],
