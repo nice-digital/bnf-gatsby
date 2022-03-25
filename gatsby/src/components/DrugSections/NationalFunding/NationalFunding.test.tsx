d203a2a0d0ac853b96517534139ef26280c90175
Ian Routledge <ian@ediblecode.com>
2022-03-25 18:48:23 +0000

    BNF-1270 Add placeholder test file

---
+++ gatsby/src/components/DrugSections/NationalFunding/NationalFunding.test.tsx
@@ -0,0 +1,7 @@
+import { screen, render } from "@testing-library/react";
+
+import { NationalFunding, type NationalFundingProps } from "./NationalFunding";
+
+describe("NationalFunding", () => {
+	// TODO
+});
