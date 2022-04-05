9dafb8191b769d7d14868e8091acad1558d2d85c
John Davey <john.davey@nice.org.uk>
2022-04-05 14:41:29 +0100

    BNF-1285 Remove unused test-utils and references to

--- gatsby/src/pages/404.test.tsx
+++ gatsby/src/pages/404.test.tsx
@@ -1,25 +1,23 @@
-import { waitFor, screen } from "@testing-library/react";
+import { render, waitFor, screen } from "@testing-library/react";
 import React from "react";
-
-import { renderWithRouter } from "@/test-utils/test-utils";
 
 import NotFoundPage from "./404";
 
 describe("404", () => {
 	it("should match snapshot", () => {
-		const { container } = renderWithRouter(<NotFoundPage />);
+		const { container } = render(<NotFoundPage />);
 
 		expect(container).toMatchSnapshot();
 	});
 
 	it("should render h1 with correct text", () => {
-		renderWithRouter(<NotFoundPage />);
+		render(<NotFoundPage />);
 
 		expect(screen.queryByText("We can't find this page")?.tagName).toBe("H1");
 	});
 
 	it("should render a noindex robots meta tag", async () => {
-		renderWithRouter(<NotFoundPage />);
+		render(<NotFoundPage />);
 
 		await waitFor(() => {
 			// eslint-disable-next-line testing-library/no-node-access
