9d375327a26d15a060041c0f2b2016a620e7113b
Eleanor Mollett <eleanor.mollett@gmail.com>
2022-04-11 15:50:27 +0100

    WIP index page

--- gatsby/src/pages/nurse-prescribers-formulary/index.tsx
+++ gatsby/src/pages/nurse-prescribers-formulary/index.tsx
@@ -1,8 +1,14 @@
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import React, { type FC } from "react";
 
-import { DetailsPageLayout } from "@/components/DetailsPageLayout/DetailsPageLayout";
-import { RecordSectionsContent } from "@/components/RecordSectionsContent/RecordSectionsContent";
+import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { ColumnList } from "@nice-digital/nds-column-list";
+import { PageHeader } from "@nice-digital/nds-page-header";
+
+import { Layout } from "@/components/Layout/Layout";
+import { SEO } from "@/components/SEO/SEO";
+import { useNursePrescribers } from "@/hooks/useNursePrescribers";
+import { useSiteMetadata } from "@/hooks/useSiteMetadata";
 import { type RecordSection } from "@/utils";
 
 export type NursePrescribersFormularyIndexPageProps = {
@@ -21,16 +27,35 @@ const NursePrescribersFormularyIndexPage: FC<
 		bnfNursePrescribersFormularyIntroduction: { title, sections },
 	},
 }) => {
+	const { siteTitleShort } = useSiteMetadata(),
+		treatmentSummaries = useNursePrescribers();
+
 	return (
-		<DetailsPageLayout
-			titleHtml={title}
-			sections={sections.map(({ slug, title }) => ({
-				id: slug,
-				title,
-			}))}
-		>
-			<RecordSectionsContent sections={sections} />
-		</DetailsPageLayout>
+		<Layout>
+			<SEO title="Nurse Prescribers' Formulary" />
+			<Breadcrumbs>
+				<Breadcrumb to="https://www.nice.org.uk/">NICE</Breadcrumb>
+				<Breadcrumb to="/" elementType={Link}>
+					{siteTitleShort}
+				</Breadcrumb>
+				<Breadcrumb>Nurse Prescribers' Formulary</Breadcrumb>
+			</Breadcrumbs>
+			<PageHeader id="content-start" heading={`Nurse Prescribers' Formulary`} />
+			{/* TODO get this properly */}
+			<Link to={`/`}>
+				Approved list for prescribing by Community Practitioner Nurse
+				Prescribers
+			</Link>
+			{/* TODO split treatmentSummaries to not have the approved list */}
+			<h2>Treatment summaries</h2>
+			<ColumnList aria-label="Pages in the about section">
+				{treatmentSummaries.map(({ href, title }) => (
+					<li key={href}>
+						<Link to={href} dangerouslySetInnerHTML={{ __html: title }} />
+					</li>
+				))}
+			</ColumnList>
+		</Layout>
 	);
 };
 
