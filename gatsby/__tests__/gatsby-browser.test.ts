87568aa8ecdae032288b73c5efa69c9d58788cf8
John Davey <66066685+johndavey72@users.noreply.github.com>
2022-04-12 08:45:38 +0100

    BNF-1255 push pageviews to the datalayer (#25)
    * BNF-1255 Add onRouteUpdate gatsby hook for more granular tracking of pageviews
    
    * unit testing wip
    
    * unit testing wip
    
    * unit testing wip
    
    * BNF-1255 Add further unit tests for data layer
    
    * unit testing wip
    
    * unit testing wip
    
    * BNF-1255 Be explicit about using requestAnimationFrame on window. Unit test wip
    
    * BNF-1255 Refactor setTimeout unit tests for better separation
    
    Co-authored-by: Ian Routledge <ian@ediblecode.com>

---
+++ gatsby/__tests__/gatsby-browser.test.ts
@@ -0,0 +1,162 @@
+import { waitFor } from "@testing-library/react";
+import { type RouteUpdateArgs } from "gatsby";
+
+import { onRouteUpdate } from "../gatsby-browser";
+
+describe("gatsby-browser", () => {
+	describe("onRouteUpdate behaviour", () => {
+		beforeEach(() => {
+			window.dataLayer = [];
+		});
+
+		afterEach(() => {
+			jest.useRealTimers();
+		});
+
+		it("should push a page view to the data layer when the path changes", async () => {
+			onRouteUpdate({
+				prevLocation: { href: "elsewhere", pathname: "/elsewhere" },
+				location: { href: "somewhere", pathname: "/somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(2);
+			});
+
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere",
+				referrer: "elsewhere",
+			});
+
+			expect(window.dataLayer[1]).toStrictEqual({
+				event: "pageview",
+			});
+		});
+
+		it("should NOT push a page view to the data layer when the path does NOT change", async () => {
+			onRouteUpdate({
+				prevLocation: { href: "somewhere", pathname: "/somewhere" },
+				location: {
+					href: "somewhere#someanchor",
+					pathname: "/somewhere",
+				},
+			} as unknown as RouteUpdateArgs);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(1);
+			});
+
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere#someanchor",
+				referrer: "somewhere",
+			});
+		});
+
+		it("Should push the prevLocation to the data layer", async () => {
+			onRouteUpdate({
+				prevLocation: {
+					href: "elsewhere",
+				},
+				location: { href: "somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(1);
+			});
+
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere",
+				referrer: "elsewhere",
+			});
+		});
+
+		it("Should push the current location to the data layer", () => {
+			onRouteUpdate({
+				prevLocation: null,
+				location: { href: "somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			expect(window.dataLayer).toHaveLength(1);
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere",
+				referrer: "",
+			});
+		});
+
+		it("should push a referrer to the data layer", () => {
+			jest.spyOn(document, "referrer", "get").mockReturnValue("some referrer");
+			onRouteUpdate({
+				prevLocation: null,
+				location: { href: "somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			expect(window.dataLayer).toHaveLength(1);
+			expect(window.dataLayer[0]).toStrictEqual({
+				location: "somewhere",
+				referrer: "some referrer",
+			});
+		});
+
+		it("Should use request animationframe delay before push to the data layer", async () => {
+			const rafSpy: jest.SpyInstance = jest
+				.spyOn(window, "requestAnimationFrame")
+				.mockImplementation((callback: FrameRequestCallback): number => {
+					callback(0);
+					return 0;
+				});
+
+			onRouteUpdate({
+				prevLocation: { href: "elsewhere", pathname: "/elsewhere" },
+				location: { href: "somewhere", pathname: "/somewhere" },
+			} as unknown as RouteUpdateArgs);
+
+			expect(rafSpy).toHaveBeenCalledTimes(2);
+
+			await waitFor(() => {
+				expect(window.dataLayer).toHaveLength(2);
+			});
+
+			rafSpy.mockRestore();
+		});
+
+		describe("setTimeout behaviour", () => {
+			const raf = window.requestAnimationFrame;
+			beforeEach(() => {
+				jest.useFakeTimers();
+				delete (window as unknown as Record<string, unknown>)
+					.requestAnimationFrame;
+			});
+
+			afterEach(() => {
+				(window as unknown as Record<string, unknown>).requestAnimationFrame =
+					raf;
+			});
+
+			it("should use set timeout when request animation frame is not available", () => {
+				onRouteUpdate({
+					prevLocation: { href: "elsewhere", pathname: "/elsewhere" },
+					location: { href: "somewhere", pathname: "/somewhere" },
+				} as unknown as RouteUpdateArgs);
+
+				expect(window.dataLayer).toHaveLength(0);
+
+				jest.runAllTimers();
+
+				expect(window.dataLayer).toHaveLength(2);
+			});
+
+			it("should call setTimeout with timing equivalent of 2 requestAnimationFrame calls", () => {
+				const setTimeoutSpy = jest.spyOn(window, "setTimeout");
+
+				onRouteUpdate({
+					prevLocation: { href: "elsewhere", pathname: "/elsewhere" },
+					location: { href: "somewhere", pathname: "/somewhere" },
+				} as unknown as RouteUpdateArgs);
+
+				jest.runAllTimers();
+
+				expect(setTimeoutSpy).toHaveBeenCalledWith(expect.any(Function), 32);
+			});
+		});
+	});
+});
