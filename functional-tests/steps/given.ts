5517db892d124e423c3819679499c43053736148
Chris Barker <chris.barker@nice.org.uk>
2022-04-13 09:47:49 +0100

    BNF-1215 Add missing cookie banner import

--- functional-tests/steps/given.ts
+++ functional-tests/steps/given.ts
@@ -3,6 +3,7 @@ import { Given } from "@cucumber/cucumber";
 import { openWebsite } from "@nice-digital/wdio-cucumber-steps/lib/support/action/openWebsite";
 import { setWindowSize } from "@nice-digital/wdio-cucumber-steps/lib/support/action/setWindowSize";
 
+import { acceptCookieBanner } from "../support/action/acceptCookieBanner";
 import { waitForReact } from "../support/action/waitForReact";
 import { getPath, PageName } from "../support/pagePaths";
 
