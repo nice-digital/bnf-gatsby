8ec5bc6b1c07b11f8076d16e18a13752e1d0e8d5
John Davey <john.davey@nice.org.uk>
2022-04-19 17:44:50 +0100

    BNF-1217 Add unit tests for A-Z drugs rendering

--- gatsby/src/pages/index.test.tsx
+++ gatsby/src/pages/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import { useStaticQuery } from "gatsby";
 
 import { type LastUpdatedDataQueryResult } from "@/components/Hero/Hero";
@@ -32,83 +32,27 @@ describe("HomePage", () => {
 		render(<HomePage />);
 		expect(screen.getByRole("main")).toMatchSnapshot();
 	});
+
+	describe("Drug list", () => {
+		it("should render an A-Z link for all letters", () => {
+			render(<HomePage />);
+			expect(screen.queryByText("A")?.tagName).toBe("A");
+			expect(screen.queryByText("B")?.tagName).toBe("A");
+			expect(screen.queryByText("D")?.tagName).toBe("A");
+			expect(screen.queryByText("K")?.tagName).toBe("A");
+			expect(screen.queryByText("Z")?.tagName).toBe("A");
+		});
+
+		it("should render letters that link to /drugs/ with appropriate anchor", () => {
+			render(<HomePage />);
+			const linkElementA = screen.getByText("A");
+			expect(linkElementA).toHaveAttribute("href", "/drugs/#a");
+
+			const linkElementH = screen.getByText("H");
+			expect(linkElementH).toHaveAttribute("href", "/drugs/#h");
+
+			const linkElementZ = screen.getByText("Z");
+			expect(linkElementZ).toHaveAttribute("href", "/drugs/#z");
+		});
+	});
 });
-
-// import React from "react";
-// import { screen } from "@testing-library/react";
-
-// import { renderWithRouter } from "test-utils";
-// import IndexPage from "../index";
-
-// const specialities = [
-// 	{
-// 		id: "86ae217e-d675-5678-91c5-7bfbaa88c2f1",
-// 		name: "Injuries",
-// 		slug: "injuries",
-// 	},
-// 	{
-// 		id: "f8332083-731e-50fe-a895-030315ce6083",
-// 		name: "Musculoskeletal",
-// 		slug: "musculoskeletal",
-// 	},
-// 	{
-// 		id: "50071c38-ab10-5ab8-ac70-6bd19a96c24e",
-// 		name: "Skin and nail",
-// 		slug: "skin-and-nail",
-// 	},
-// ];
-
-// const topics = [
-// 	"Achilles tendinopathy",
-// 	"Back pain - low (without radiculopathy)",
-// 	"DMARDs",
-// ];
-
-// const topicAliases = [
-// 	"Kidney injury - acute",
-// 	"Z-drug and benzodiazepine withdrawal",
-// ];
-
-// describe("Home page", () => {
-// 	beforeEach(() => {
-// 		renderWithRouter(
-// 			<IndexPage
-// 				data={{
-// 					allSpecialities: { nodes: specialities },
-// 					allTopicNames: { distinct: topics },
-// 					allTopicNameAliases: { distinct: topicAliases },
-// 				}}
-// 			/>
-// 		);
-// 	});
-
-// 	it("should render the titles with the expected text", () => {
-// 		expect(screen.queryByText("Clinical Knowledge Summaries")?.tagName).toBe(
-// 			"H1"
-// 		);
-// 		expect(screen.queryByText("Health topics A to Z")?.tagName).toBe("H2");
-// 		expect(screen.queryByText("Specialities")?.tagName).toBe("H2");
-// 	});
-
-// 	describe("Topic list", () => {
-// 		it("should render an A-Z link for any topics and aliases that begin with that letter", () => {
-// 			expect(screen.queryByText("A")?.tagName).toBe("A");
-// 			expect(screen.queryByText("B")?.tagName).toBe("A");
-// 			expect(screen.queryByText("D")?.tagName).toBe("A");
-// 			expect(screen.queryByText("K")?.tagName).toBe("A");
-// 			expect(screen.queryByText("Z")?.tagName).toBe("A");
-// 		});
-
-// 		it("should not provide an A-Z link if there are no topics beginning with that letter", () => {
-// 			expect(screen.queryByText("C")?.tagName).toBe("SPAN");
-// 		});
-// 	});
-
-// 	describe("Specialty list", () => {
-// 		it("should show any supplied specialities", () => {
-// 			specialities.map(({ name }) => {
-// 				expect(screen.queryByText(name)).toBeInTheDocument();
-// 			});
-// 		});
-// 	});
-// });
