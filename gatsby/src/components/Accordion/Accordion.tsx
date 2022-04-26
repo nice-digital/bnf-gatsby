3580fd7bd4cd3467f47325f394f6e14be8c8dbf5
Chris Barker <chris@christopherbarker.co.uk>
2022-04-26 09:54:33 +0100

    BNF-1215 Medicinal forms (#31)
    * BNF-1215 WIP Medicinal form styling
    
    * BNF-1215 WIP Medicinal forms styles and node creation
    
    * BNF-215 Link labels to forms; add temporary additionalNotes label
    
    * BNF-1215 WIP medicinal forms rendering and tests
    
    * BNF-1215 Fix merge errors
    
    * BNF-1215 Refactor medicinal form types; update styling
    
    * BNF-1215 Code updates post code review; simplified stylelint rules
    
    * BNF-1215 Add warning icon to cautionary labels
    
    * BNF-1215 Add excipients, electrolytes and hospital-only prices
    
    * BNF-1215 Fix issue with nested accordions
    
    * BNF-1215 Fix snapshots, add more unit tests
    
    * BNF-1215 Add functional tests for edge case medicinal form pages
    
    * BNF-1215 Refactor medicinal forms functional tests
    
    * BNF-1215 Update tests post code-review
    
    * BNF-1215 Remove co-codamol page from functional tests
    
    * BNF-1215 Check for existence of treatment summaries when creating NPF nodes
    
    * BNF-1215 Fix functional test errors caused by capitalisation of drug names
    
    * BNF-1215 Rename electrolytes after feed corrected typo
    
    * BNF-1215 Render qualifier in medicinal form labels
    
    * BNF-1215 Update index schema snapshot
    
    * BNF-1215 Use new design system var to specify readable width
    
    * BNF-1215 Remove functional test with false positive for a11y contrast check
    
    * BNF-1215 Remove eslint/TS warnings
    
    * BNF-1215 Update TS path resolution
    
    * Revert "BNF-1215 Update TS path resolution"
    
    This reverts commit d38e76601a1ab50183ca8629b21daecf2b4dfd1c.
    
    * BNF-1215 Resolve NDS/Gatsby dependency issues
    
    * BNF-1215 Update warning colours and medicinal form schema post code review
    
    * BNF-1215 Resolve merge error
    
    * BNF-1215 Add missing cookie banner import
    
    * BNF-1215 Bump version of wdio-cucumber-steps
    
    * BNF-1215 Replace acceptCookieBanner reference
    
    * BNF-1215 Apply design system patch
    
    * BNF-1215 Fix lint errors
    
    * BNF-1215 Decrease manufacturer font weight
    
    * BNF-1215 Add controlled schedule and legal category symbols
    
    * BNF-1215 Update medicinal form tests; add snapshot
    
    * BNF-1215 Refactor
    
    * BNF-125 Fix unit tests
    
    * BNF-1215 Add meta descriptions and tests for medicinal forms
    
    * BNF-1215 Medicinal form accordion design updates
    
    * BNF-1215 Prevent encoding of characters within monograph link
    
    * BNF-1215 Update snapshots
    
    * BNF-1215 Fix label icon alignment
    
    Co-authored-by: Chris Barker <chris.barker@nice.org.uk>
    Co-authored-by: Ian Routledge <ian@ediblecode.com>

--- gatsby/src/components/Accordion/Accordion.tsx
+++ gatsby/src/components/Accordion/Accordion.tsx
@@ -4,12 +4,17 @@ import ChevronDownIcon from "@nice-digital/icons/lib/ChevronDown";
 
 import styles from "./Accordion.module.scss";
 
+export enum AccordionTheme {
+	Warning,
+}
 export interface AccordionProps {
 	title: ReactNode;
 	children: ReactNode;
 	defaultOpen?: boolean;
 	showLabel?: string;
 	hideLabel?: ReactNode;
+	className?: string;
+	theme?: AccordionTheme;
 }
 
 export const Accordion: FC<AccordionProps> = ({
@@ -18,13 +23,18 @@ export const Accordion: FC<AccordionProps> = ({
 	defaultOpen = false,
 	showLabel = "Show",
 	hideLabel = "Hide",
+	className,
+	theme,
 }) => {
 	const [isOpen, setIsOpen] = useState(defaultOpen);
+	const themeClass =
+		theme === AccordionTheme.Warning ? styles.warningTheme : "";
 
 	return (
 		<details
-			className={styles.details}
-			onToggle={() => {
+			className={[styles.details, className, themeClass].join(" ")}
+			onToggle={(e) => {
+				e.stopPropagation(); // Ensure event isn't passed to parent accordions
 				setIsOpen((isOpen) => !isOpen);
 			}}
 			open={defaultOpen}
