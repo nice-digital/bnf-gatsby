0c861b4dfa498f614ca0086560c7a09797b47eb6
Chris Barker <chris.barker@nice.org.uk>
2022-04-27 16:28:43 +0100

    BNF-1239 WIP Wound management taxonomy page

--- gatsby/src/pages/wound-management/{BnfWoundManagementTaxonomy.slug}/index.tsx
+++ gatsby/src/pages/wound-management/{BnfWoundManagementTaxonomy.slug}/index.tsx
@@ -2,22 +2,36 @@ import { graphql, Link } from "gatsby";
 import React, { type FC } from "react";
 
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
+import { Grid, GridItem } from "@nice-digital/nds-grid";
 import { PageHeader } from "@nice-digital/nds-page-header";
+import { StackedNav, StackedNavLink } from "@nice-digital/nds-stacked-nav";
 
 import { Layout } from "@/components/Layout/Layout";
+import {
+	SectionNav,
+	type SectionNavProps,
+} from "@/components/SectionNav/SectionNav";
 import { SEO } from "@/components/SEO/SEO";
 import { useSiteMetadata } from "@/hooks/useSiteMetadata";
 
+import styles from "./index.module.scss";
+
 export interface WoundManagementTaxonomyPageProps {
 	data: {
+		allBnfWoundManagementTaxonomy: {
+			taxonomies: {
+				title: string;
+				slug: string;
+			}[];
+		};
 		bnfWoundManagementTaxonomy: {
 			title: string;
 			slug: string;
-			text: string;
+			text: string | null;
 			childTaxonomies: {
 				title: string;
 				slug: string;
-				text: string;
+				text: string | null;
 				childTaxonomies: {
 					title: string;
 					slug: string;
@@ -29,10 +43,19 @@ export interface WoundManagementTaxonomyPageProps {
 
 const WoundManagementTaxonomyPage: FC<WoundManagementTaxonomyPageProps> = ({
 	data: {
+		allBnfWoundManagementTaxonomy: { taxonomies },
 		bnfWoundManagementTaxonomy: { slug, title, text, childTaxonomies },
 	},
 }) => {
 	const { siteTitleShort } = useSiteMetadata();
+	const navSections: SectionNavProps = {
+		sections: childTaxonomies.map(({ slug, title }) => {
+			return {
+				id: slug,
+				title,
+			};
+		}),
+	};
 
 	return (
 		<Layout>
@@ -57,45 +80,77 @@ const WoundManagementTaxonomyPage: FC<WoundManagementTaxonomyPageProps> = ({
 				heading={<span dangerouslySetInnerHTML={{ __html: title }} />}
 			/>
 
-			<div dangerouslySetInnerHTML={{ __html: text }}></div>
+			<Grid gutter="loose" data-testid="body">
+				<GridItem cols={12} md={4} lg={3} className="hide-print">
+					<StackedNav
+						aria-label="Wound management product pages"
+						label="Wound management products"
+						link={{ destination: "/wound-management/", elementType: Link }}
+					>
+						{taxonomies.map(({ slug, title }) => (
+							<StackedNavLink
+								key={slug}
+								destination={`/wound-management/${slug}`}
+								elementType={Link}
+							>
+								<span dangerouslySetInnerHTML={{ __html: title }} />
+							</StackedNavLink>
+						))}
+					</StackedNav>
+				</GridItem>
+				<GridItem cols={12} md={8} lg={9}>
+					<SectionNav {...navSections} />
+					{text && <div dangerouslySetInnerHTML={{ __html: text }}></div>}
 
-			{childTaxonomies.length > 0 && (
-				<ul>
-					{childTaxonomies.map((child) => (
-						<>
-							<h2>{child.title}</h2>
-							<div dangerouslySetInnerHTML={{ __html: child.text }}></div>
-							{child.childTaxonomies.length > 0 ? (
-								<ul>
-									{child.childTaxonomies.map((nestedChild) => (
-										<li key={nestedChild.slug}>
-											<Link
-												to={`/wound-management/${slug}/${nestedChild.slug}`}
-											>
-												{nestedChild.title}
-											</Link>
-										</li>
-									))}
-								</ul>
-							) : (
-								<ul>
-									<li>
-										<Link to={`/wound-management/${slug}/${child.slug}`}>
-											{child.title}
-										</Link>
-									</li>
-								</ul>
-							)}
-						</>
-					))}
-				</ul>
-			)}
+					{childTaxonomies.length > 0 && (
+						<ul className={styles.childTaxonomyList}>
+							{childTaxonomies.map((child) => (
+								<li key={child.slug}>
+									<h2 id={child.slug}>{child.title}</h2>
+									{child.text && (
+										<div dangerouslySetInnerHTML={{ __html: child.text }}></div>
+									)}
+									{child.childTaxonomies.length > 0 ? (
+										<ul className={styles.nestedTaxonomyList}>
+											{child.childTaxonomies.map((nestedChild) => (
+												<li key={nestedChild.slug}>
+													<Link
+														to={`/wound-management/${slug}/${nestedChild.slug}`}
+													>
+														{nestedChild.title}
+													</Link>
+												</li>
+											))}
+										</ul>
+									) : (
+										<ul className={styles.nestedTaxonomyList}>
+											<li>
+												<Link to={`/wound-management/${slug}/${child.slug}`}>
+													{child.title}
+												</Link>
+											</li>
+										</ul>
+									)}
+								</li>
+							))}
+						</ul>
+					)}
+				</GridItem>
+			</Grid>
 		</Layout>
 	);
 };
 
 export const query = graphql`
 	query ($id: String) {
+		allBnfWoundManagementTaxonomy(
+			filter: { parentTaxonomy: { title: { eq: null } } }
+		) {
+			taxonomies: nodes {
+				title
+				slug
+			}
+		}
 		bnfWoundManagementTaxonomy(id: { eq: $id }) {
 			title
 			slug
