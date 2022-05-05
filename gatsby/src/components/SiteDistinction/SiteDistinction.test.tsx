d7b1037e2365a5368a995ca442bcd7daf8d99885
John Davey <john.davey@nice.org.uk>
2022-05-05 10:31:03 +0100

    unit testing wip

--- gatsby/src/components/SiteDistinction/SiteDistinction.test.tsx
+++ gatsby/src/components/SiteDistinction/SiteDistinction.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen, within } from "@testing-library/react";
 
+import { useSiteMetadata } from "@/hooks/useSiteMetadata";
+
 import { SiteDistinction } from "./SiteDistinction";
 
 describe("SiteDistinction", () => {
@@ -8,18 +10,47 @@ describe("SiteDistinction", () => {
 		expect(screen.getByLabelText("Show BNFC link")).toMatchSnapshot();
 	});
 
-	it("should render a button with an accessible label to show bnfc link", () => {
-		render(<SiteDistinction />);
-		const button = screen.getByRole("button");
-		expect(button).toHaveAccessibleName("Show BNFC link");
-	});
+	describe.each([
+		["BNF", true, "BNFC"],
+		["BNFC", false, "BNF"],
+	])("%s site", (siteTitleShort, isBNF, otherSiteTitleShort) => {
+		beforeEach(() => {
+			(useSiteMetadata as jest.Mock).mockReturnValue({
+				siteTitleShort,
+				isBNF,
+			});
+		});
 
-	it("should render visually hidden content for site switch", () => {
-		render(<SiteDistinction />);
+		describe("Button", () => {
+			it.todo("should not render a button on serverside");
+			it(`should render a button with an accessible label to show ${otherSiteTitleShort} link`, () => {
+				render(<SiteDistinction />);
+				expect(
+					screen.getByRole("button", {
+						name: `Show ${otherSiteTitleShort} link`,
+					})
+				).toBeInTheDocument();
+			});
+			it.todo("should not be expanded by default");
+			it.todo("should expand on click");
+			it.todo("should change text to hide other site link on click");
+			it.todo("should render an icon with an expanded class");
+		});
 
-		const hiddenSpan = within(
-			screen.getByRole("link", { name: "switch to BNFC" })
-		).getByText("switch to");
-		expect(hiddenSpan).toHaveClass("visually-hidden");
+		describe("Link", () => {
+			it.todo("should ");
+			it.todo("should have an href with ref = switch on querystring");
+		});
+
+		describe("Visually hidden", () => {
+			it(`should render visually hidden content for ${otherSiteTitleShort} site switch`, () => {
+				render(<SiteDistinction />);
+
+				const hiddenSpan = within(
+					screen.getByRole("link", { name: `switch to ${otherSiteTitleShort}` })
+				).getByText("switch to");
+				expect(hiddenSpan).toHaveClass("visually-hidden");
+			});
+		});
 	});
 });
