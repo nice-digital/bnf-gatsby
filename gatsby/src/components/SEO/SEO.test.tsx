f76e2b388667624e2c15927ac646f12ed0d8457d
Ian Routledge <ian@ediblecode.com>
2022-05-10 10:18:00 +0100

    BNF-1193 Add theme colour tests

--- gatsby/src/components/SEO/SEO.test.tsx
+++ gatsby/src/components/SEO/SEO.test.tsx
@@ -111,4 +111,26 @@ describe("SEO", () => {
 			);
 		});
 	});
+
+	it("should render theme colour meta tag for BNF", async () => {
+		render(<SEO />);
+		await waitFor(() => {
+			expect(
+				document.querySelector("meta[name='theme-color']")
+			).toHaveAttribute("content", "#93da49");
+		});
+	});
+
+	it("should render theme colour meta tag for BNF", async () => {
+		(useSiteMetadata as jest.Mock).mockReturnValueOnce({
+			...useSiteMetadata(),
+			isBNF: false,
+		});
+		render(<SEO />);
+		await waitFor(() => {
+			expect(
+				document.querySelector("meta[name='theme-color']")
+			).toHaveAttribute("content", "#ef4a81");
+		});
+	});
 });
