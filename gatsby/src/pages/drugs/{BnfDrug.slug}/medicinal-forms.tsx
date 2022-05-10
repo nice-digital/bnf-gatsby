df1746d71a1367bb570dc1ab9bd953bb28d9618c
John Davey <john.davey@nice.org.uk>
2022-05-10 15:05:29 +0100

    BNF-1303 Add full stop to drug sales disclaimer

--- gatsby/src/pages/drugs/{BnfDrug.slug}/medicinal-forms.tsx
+++ gatsby/src/pages/drugs/{BnfDrug.slug}/medicinal-forms.tsx
@@ -46,7 +46,7 @@ export interface MedicinalFormsPageProps {
 
 const asideInfo: ReactElement = (
 	<Alert>
-		<p>NICE does not sell any drugs, medicines or pharmaceutical products</p>
+		<p>NICE does not sell any drugs, medicines or pharmaceutical products.</p>
 	</Alert>
 );
 
