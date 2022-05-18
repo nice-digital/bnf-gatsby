58d330ef9843456c54bfee1275d7905480e6544b
Chris Barker <chris.barker@nice.org.uk>
2022-05-18 17:44:32 +0100

    BNF-1247 WIP sticky section nav

--- gatsby/src/components/SectionNav/SectionNav.tsx
+++ gatsby/src/components/SectionNav/SectionNav.tsx
@@ -1,8 +1,9 @@
 import classNames from "classnames";
 import { Link } from "gatsby";
-import { FC } from "react";
+import { FC, useState, useRef } from "react";
 import striptags from "striptags";
 
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import { isTruthy } from "@/utils";
 
 import styles from "./SectionNav.module.scss";
@@ -24,31 +25,58 @@ export const SectionNav: FC<SectionNavProps> = ({
 	className,
 	readableMaxWidth = false,
 	navigateToAnotherPage = false,
-}) => (
-	<nav
-		aria-labelledby="navigate-to-section"
-		className={classNames(
-			styles.nav,
-			readableMaxWidth ? styles.navReadableMaxWidth : null,
-			className
-		)}
-	>
-		<h2 id="navigate-to-section" className={styles.heading}>
-			{navigateToAnotherPage ? "Navigate to page" : "Navigate to section"}
-		</h2>
-		<ol
-			aria-label="Jump links to sections on this page"
-			className={styles.linkList}
+}) => {
+	// const [isDropdownVisible, setIsDropdownVisible] = useState(true);
+	const headingText = navigateToAnotherPage
+		? "Navigate to page"
+		: "Navigate to section";
+
+	const ref = useRef<HTMLDivElement | null>(null);
+	const entry = useIntersectionObserver(ref, {
+		rootMargin: "-1px 0px",
+		threshold: 1,
+	});
+	const isStuck = !entry?.isIntersecting;
+	console.log("Is stuck?", isStuck);
+
+	const toggleDropdown = () => {
+		console.log("TOGGLE TIME!");
+	};
+
+	return (
+		<nav
+			aria-labelledby="navigate-to-section"
+			className={classNames(
+				styles.nav,
+				readableMaxWidth ? styles.navReadableMaxWidth : null,
+				isStuck ? styles.stuck : null,
+				className
+			)}
+			ref={ref}
 		>
-			{sections.filter(isTruthy).map((section) => (
-				<li key={section?.id}>
-					{navigateToAnotherPage ? (
-						<Link to={section?.id}>{striptags(section?.title || "")}</Link>
-					) : (
-						<a href={`#${section?.id}`}>{striptags(section?.title || "")}</a>
-					)}
-				</li>
-			))}
-		</ol>
-	</nav>
-);
+			<h2 id="navigate-to-section" className={styles.heading}>
+				{isStuck ? (
+					<button type="button" onClick={toggleDropdown}>
+						{headingText}
+					</button>
+				) : (
+					<span>{headingText}</span>
+				)}
+			</h2>
+			<ol
+				aria-label="Jump links to sections on this page"
+				className={styles.linkList}
+			>
+				{sections.filter(isTruthy).map((section) => (
+					<li key={section?.id}>
+						{navigateToAnotherPage ? (
+							<Link to={section?.id}>{striptags(section?.title || "")}</Link>
+						) : (
+							<a href={`#${section?.id}`}>{striptags(section?.title || "")}</a>
+						)}
+					</li>
+				))}
+			</ol>
+		</nav>
+	);
+};
