da7870f5f7a605322f773f37594e22b698673823
Eleanor Mollett <eleanor.mollett@gmail.com>
2022-05-24 11:54:33 +0100

    Use svg

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
 				background_color: `#fff`,
 				theme_color: isBNF ? "#93da49" : "#ef4a81",
 				display: `minimal-ui`,
-				icon: `src/images/favicon.ico`,
+				icon: `src/images/logo-square.svg`,
 				icon_options: {
 					purpose: `maskable any`,
 				},
