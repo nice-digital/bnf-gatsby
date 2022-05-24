5c733ecc41bd4b2356e8bc7688c651ce946ffb76
John Davey <john.davey@nice.org.uk>
2022-05-24 12:35:16 +0100

    BNF-1247 Remove unused deps from unit test

--- gatsby/src/components/SectionNav/SectionNav.test.tsx
+++ gatsby/src/components/SectionNav/SectionNav.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { renderToString } from "react-dom/server";
 
