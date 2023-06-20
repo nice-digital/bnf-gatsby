9b605eb4149b4392ccaca68ee8b3551509ca1ec2
John Davey <john.davey@nice.org.uk>
2023-06-20 10:40:10 +0100

    BNF-1416 Remove debug prop from GlobalNavHeader

--- gatsby/src/components/SiteHeader/SiteHeader.tsx
+++ gatsby/src/components/SiteHeader/SiteHeader.tsx
@@ -82,7 +82,6 @@ export const SiteHeader: React.FC = () => {
 	return (
 		<div ref={globalNavWrapperRef}>
 			<GlobalNavHeader
-				renderSearchOnly={true}
 				service={isBNF ? "bnf" : "bnfc"}
 				skipLinkId="content-start"
 				onNavigating={(e): void => {
