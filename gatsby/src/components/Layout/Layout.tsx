eb552006ec9029d1241b4b5e7961dc30520eb5c7
Chris Barker <chris.barker@nice.org.uk>
2024-06-28 11:25:08 +0100

    BNF-1457 Fix snapshots, remove IE banner

--- gatsby/src/components/Layout/Layout.tsx
+++ gatsby/src/components/Layout/Layout.tsx
@@ -3,11 +3,8 @@ import React, { ReactNode } from "react";
 import { Footer, Main } from "@nice-digital/global-nav";
 import { Container } from "@nice-digital/nds-container";
 
-import { useIsClient } from "@/hooks/useIsClient";
-
 import { EULABanner } from "../EULABanner/EULABanner";
 import { EULAPanel } from "../EULAPanel/EULAPanel";
-import { IEBanner } from "../IEBanner/IEBanner";
 import { SiteDistinction } from "../SiteDistinction/SiteDistinction";
 import { SiteHeader } from "../SiteHeader/SiteHeader";
 
@@ -29,16 +26,8 @@ const shouldShowEULABanner =
 	process.env.GATSBY_HIDE_EULA_BANNER === "true" ? false : true;
 
 export const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
-	const isClient = useIsClient();
-	let isIE = false;
-	if (isClient) {
-		const doc: Document = window.document;
-		isIE = !!doc.documentMode;
-	}
-
 	return (
 		<>
-			{isIE && <IEBanner />}
 			{shouldShowEULABanner && <EULABanner />}
 			<SiteHeader />
 			<Main>
