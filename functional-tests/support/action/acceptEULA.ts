b4c948951e440721efe53012a077e99329409920
Chris Barker <chris.barker@nice.org.uk>
2024-06-28 13:48:41 +0100

    BNF-1457 Remove EULA wait to exist step from func tests

--- functional-tests/support/action/acceptEULA.ts
+++ functional-tests/support/action/acceptEULA.ts
@@ -1,6 +1,5 @@
 export async function acceptEULA(): Promise<void> {
 	const EULAAcceptButton = await $("#btn-accept-bnf-eula");
-	await EULAAcceptButton.waitForExist({ timeout: 5000 });
 
 	if (await EULAAcceptButton.isDisplayed()) await EULAAcceptButton.click();
 }
