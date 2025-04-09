284af7f4d591a61b71c5b05f4191b975db93a590
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-04-09 15:11:26 +0100

    BNF-1466 Add styles according to figma design

--- gatsby/src/components/ErrorPageContent/ErrorPageContent.tsx
+++ gatsby/src/components/ErrorPageContent/ErrorPageContent.tsx
@@ -2,17 +2,24 @@ import { Announcer } from "@/components/Announcer/Announcer";
 
 import { SEO } from "../SEO/SEO";
 
+import styles from "./ErrorPageContent.module.scss";
+
 export const ErrorPageContent: React.FC = () => {
 	// TODO error page content
 	return (
 		<>
 			<SEO title={`Error`} />
 			<Announcer announcement="An error occurred" />
-			<p>
-				Sorry, there has been a problem, and we cannot load the page you are
-				looking for. Please try again.
-			</p>
-			<p>
+			{/* <PageHeader
+				heading={"Sorry, there has been a problem"}
+				lead={"We cannot load the page you are looking for"}
+				id="content-start"
+				verticalPadding="loose"
+			/> */}
+			<h1 className={styles.errorHeading}>Sorry, there has been a problem</h1>
+			<p className="lead">We cannot load the page you are looking for.</p>
+			<p className="lead">Please try again.</p>
+			<p className="body">
 				If you get this message more than once, please report it using our
 				feedback form:
 				<a href="https://www.nice.org.uk/forms/leave-feedback">
